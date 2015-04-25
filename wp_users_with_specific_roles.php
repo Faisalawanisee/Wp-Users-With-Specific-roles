37b2ba52146eb507a843ac3d3f0961320777b2e5
Faisal Awan <faisalawan.live@gmail.com>
2015-04-26 01:56:27 +0500

    Update wp_users_with_specific_roles.php

--- wp_users_with_specific_roles.php
+++ wp_users_with_specific_roles.php
@@ -1,6 +1,11 @@
 <?php
 /*
-Plugin Name: Wp Users With Specific roles
+Plugin Name: Wp Users With Specific roles Widget
+Plugin URI: https://github.com/Faisalawanisee/Wp-Users-With-Specific-roles
+Description: This plugin show all users with specific roles in widget 
+Author: Faisal Awan
+Version: 1.0
+Author URI: https://facebook.com/Faisalawanisee
 */
 
 /**
@@ -8,25 +13,14 @@ Plugin Name: Wp Users With Specific roles
  */
 class users_get_widget extends WP_Widget {
 
-	/**
-	 * Register widget with WordPress.
-	 */
 	function __construct() {
 		parent::__construct(
-			'users_get_widget', // Base ID
-			__( 'Users', 'text_domain' ), // Name
-			array( 'description' => __( 'Wp Users Get With Specific roles', 'text_domain' ), ) // Args
+			'users_get_widget',
+			__( 'Users', 'wpuwsrw' ),
+			array( 'description' => __( 'Wp Users Get With Specific roles', 'wpuwsrww' ), )
 		);
 	}
 
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
@@ -51,15 +45,8 @@ class users_get_widget extends WP_Widget {
 		echo $args['after_widget'];
 	}
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Users', 'wpuwsrw' );
 		$users_instance = $instance['users']; ?>
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
@@ -69,23 +56,13 @@ class users_get_widget extends WP_Widget {
 		<?php global $wp_roles; ?>
 		<?php foreach ( $wp_roles->roles as $key=>$value ): ?>
 		<p>
-			<input <?php if(in_array($key, $users_instance)){ echo 'checked '; } ?> type="checkbox" id="user-<?php echo $key; ?>-<?php echo $this->get_field_id( 'users' ); ?>" value="<?php echo $key; ?>" name="<?php echo $this->get_field_name( 'users' ); ?>[]" />
+			<input <?php if($users_instance && in_array($key, $users_instance)){ echo 'checked="checked" '; } ?> type="checkbox" id="user-<?php echo $key; ?>-<?php echo $this->get_field_id( 'users' ); ?>" value="<?php echo $key; ?>" name="<?php echo $this->get_field_name( 'users' ); ?>[]" />
 			<label for="user-<?php echo $key; ?>-<?php echo $this->get_field_id( 'users' ); ?>"><?php echo $value['name']; ?></label>
-		</p>	 
+		</p>
 		<?php endforeach; ?>
 		<?php 
 	}
 
-	/**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
@@ -100,3 +77,8 @@ function register_users_get_widget() {
     register_widget( 'users_get_widget' );
 }
 add_action( 'widgets_init', 'register_users_get_widget' );
+
+
+
+
+
